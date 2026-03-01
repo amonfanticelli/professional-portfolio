1e2238fcb0671eb7cb66d666a4bfbc94fdf12ae6
Amon Fanticelli <amon.fanticelli@wicomm.com.br>
2026-02-28 23:19:09 -0300

    feat: header bg, header profile pic and text

---
+++ tailwind.config.ts
@@ -0,0 +1,26 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      animation: {
+        gradient: "gradientBG 15s ease infinite",
+      },
+      keyframes: {
+        gradientBG: {
+          "0%": { backgroundPosition: "0% 50%" },
+          "50%": { backgroundPosition: "100% 50%" },
+          "100%": { backgroundPosition: "0% 50%" },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;

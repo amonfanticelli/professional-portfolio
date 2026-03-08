c1b76bc8af227cc1921253ee845b0a51f0a2c27e
Amon Fanticelli <amon.fanticelli@wicomm.com.br>
2026-03-08 12:39:57 -0300

    style: animation added on text

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+"use client";
 import profileImage from "./assets/profile-image.jpg";
 import Image from "next/image";
+import { useReveal } from "./components/useReveal";
 
 export default function Home() {
+   useReveal()
   return (
     <header
       className="
@@ -13,7 +16,7 @@ export default function Home() {
       "
     >
       <div className="max-w-300 mx-auto px-5 w-full relative z-10 flex items-center justify-between flex-wrap gap-10">
-        <div className="flex flex-row">
+        <div className="flex flex-row gap-8">
           <div className="relative shrink-0 gap-8 profile-image-container">
             <Image
               src={profileImage}
@@ -34,10 +37,10 @@ export default function Home() {
             />
           </div>
 
-          <div className="flex flex-col">
-            <h1>Amon Fanticelli</h1>
-            <span> Dev Full-Stack • Data Science • Game Dev </span>
-            <span>
+          <div className="flex flex-col max-w-lg">
+            <h1  style={{ transitionDelay: "0ms" }}className=" text-5xl font-bold mb-3 break-normal text-white text-shadow-[0_4px_12px_rgba(0,0,0,0.1)] reveal" >Amon Fanticelli</h1>
+            <span  style={{ transitionDelay: "200ms" }} className=" max-w-max text-[1.4rem] text-white/95 mb-3 font-semibold inline-block bg-white/10 px-3 py-1 rounded-full backdrop-blur-sm reveal"> Dev Full-Stack • Data Science • Game Dev </span>
+            <span style={{ transitionDelay: "400ms" }} className="reveal" >
               De jogos interativos a pipelines de dados e aplicações web
               completas — construo soluções criativas que combinam código limpo,
               design inteligente e análise robusta.
